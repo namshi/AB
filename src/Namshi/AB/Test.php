94a5a48404cdd8cbe83ad4feb7869ebb5e1512f5
odino <alessandro.nadalin@gmail.com>
2013-07-12 10:12:04 +0400

    implemented the Test class and added a deep test for it...pretty funny thing is that tests are based on probability, so there *MIGHT* be a chance they can fail...

--- src/Namshi/AB/Test.php
+++ src/Namshi/AB/Test.php
@@ -2,13 +2,21 @@
 
 namespace Namshi\AB;
 
-class Test
+use Countable;
+use InvalidArgumentException;
+use BadMethodCallException;
+
+class Test implements Countable
 {
     protected $name;
+    protected $variations   = array();
+    protected $isEnabled    = true;
+    protected $hasRun       = false;
     
-    public function __construct($name)
+    public function __construct($name, array $variations = array())
     {
         $this->setName($name);
+        $this->setVariations($variations);
     }
     
     public function getName()
@@ -19,5 +27,113 @@ class Test
     public function setName($name)
     {
         $this->name = $name;
+    }
+    
+    public function count()
+    {
+        return count($this->variations);
+    }
+    
+    public function getVariations()
+    {
+        return $this->variations;
+    }
+
+    public function setVariations(array $variations)
+    {
+        $this->validateVariations($variations);
+        $this->variations = $variations;
+    }
+    
+    public function disable()
+    {
+        $this->isEnabled = false;
+    }
+    
+    public function isEnabled()
+    {
+        return $this->isEnabled;
+    }
+    
+    public function isDisabled()
+    {
+        return !$this->isEnabled();
+    }
+    
+    public function run()
+    {
+        $this->hasRun(true);
+    }
+    
+    public function getVariation()
+    {
+        if ($this->hasRun()) {
+            if ($this->isDisabled()) {
+                $variations = array_keys($this->getVariations());
+
+                return array_shift($variations);
+            }
+            
+            return $this->calculateVariation();
+        }
+        
+        throw new BadMethodCallException("You must run() the test before getting its variation");
+    }
+    
+    public function hasRun($ran = null)
+    {
+        if (is_bool($ran)) {
+            $this->hasRun = $ran;
+        }
+        
+        return $this->hasRun;
+    }
+    
+    protected function validateVariations(array $variations)
+    {
+        array_walk($variations, function($variation) {
+            if (!is_int($variation)) {
+                throw new InvalidArgumentException;
+            }
+        });
+    }
+    
+    protected function calculateVariation()
+    {
+        $random = mt_rand(1, 100);
+        $odds   = $this->calculateOddsAsPercentage();
+        
+        foreach ($odds as $variation => $odd) {
+            if ($random > $odd['min'] && $random <= $odd['max']) {
+                return $variation;
+            }
+        }
+    }
+    
+    protected function calculateOddsAsPercentage()
+    {
+        $odds = array();
+        $min  = 0;
+        $max  = 0;
+        
+        foreach ($this->getVariations() as $variation => $odd) {
+            $odd = number_format($odd * 100 / array_sum($this->getVariations()), 0);
+            $max += $odd;
+ 
+            $odds[$variation] = array(
+                'value' => $odd,
+                'min'   => $min,
+                'max'   => $max,
+            );
+            
+            $min += $odd;
+            
+        }
+        
+        $variations                     = array_keys($odds);
+        $lastVariation                  = end($variations);
+        $odds[$lastVariation]['max']    = 100;
+ 
+        return $odds;
     }
 }
--- tests/Namshi/AB/Test/TestTest.php
+++ tests/Namshi/AB/Test/TestTest.php
@@ -6,13 +6,143 @@ use Namshi\AB\Test;
 
 class TestTest extends \PHPUnit_Framework_TestCase
 {
-    public function setup()
+    /**
+     * @return Namshi\AB\Test
+     */
+    public function getTest($name = 'myTest', array $variations = array('a' => 0, 'b' => 1))
     {
-        $this->test = new Test('myTest');
+        return new Test($name, $variations);
     }
     
     public function testTheTestsFirstArgumentIsItsName()
     {
-        $this->assertEquals('myTest', $this->test->getName());
+        $this->assertEquals('myTest', $this->getTest()->getName());
+    }
+    
+    public function testTheTestCanHaveMultipleVariations()
+    {
+        $this->assertCount(2, $this->getTest()->getVariations());
+    }
+    
+    public function testCountingTheVariations()
+    {
+        $this->assertCount(2, $this->getTest());
+    }
+    
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testValidatingTheVariations()
+    {
+        $this->getTest('myTest', array('a' => 'b'));
+    }
+    
+    public function testTheTestIsEnabledByDefault()
+    {
+        $this->assertTrue($this->getTest()->isEnabled());
+        $this->assertFalse($this->getTest()->isDisabled());
+    }
+    
+    public function testDisablingTheTest()
+    {        
+        for ($i = 0; $i < 100; $i++) {
+            $test = $this->getTest('myTest', array('a' => 0, 'b' => 1));
+            $test->disable();
+
+            $this->assertFalse($test->isEnabled());
+            $this->assertTrue($test->isDisabled());
+            $test->run();
+            
+            $this->assertEquals('a', $test->getVariation());
+        }
+    }
+    
+    /**
+     * @expectedException BadMethodCallException
+     */
+    public function testGettingAVariationOfATestThatHasntRunThrowsAnException()
+    {
+        $this->getTest()->getVariation();
+    }
+    
+    public function testCheckingIfATestHasRun()
+    {
+        $test = $this->getTest();
+        
+        $this->assertFalse($test->hasRun());
+        
+        $test->run();
+        
+        $this->assertTrue($test->hasRun());
+        
+        $test->hasRun(false);
+        
+        $this->assertFalse($test->hasRun());
+        
+        $test->hasRun(true);
+        
+        $this->assertTrue($test->hasRun());
+    }
+    
+    public function testGettingTheVariationOfATestWithAVariationWithProbability100()
+    {
+        $test = $this->getTest();
+        
+        $test->run();
+        
+        for ($i = 0; $i < 1000; $i++) {
+            $this->assertEquals('b', $test->getVariation());
+        }
+    }
+    
+    public function testGettingTheVariationOfATestWithOnlyOneVariation()
+    {
+        $test = $this->getTest('test', array('a' => 2));
+        
+        $test->run();
+        
+        for ($i = 0; $i < 1000; $i++) {
+            $this->assertEquals('a', $test->getVariation());
+        }
+    }
+    
+    public function testGettingTheVariationOfATestWithSplitOddsBetweenTwoVariations()
+    {
+        $tries  = 100000;
+        $test   = $this->getTest('test', array('a' => 1, 'b' => 1));
+        $test->run();
+        $counts = array('a' => 0, 'b' => 0);
+        
+        for ($i = 0; $i < $tries; $i++) {
+            $counts[$test->getVariation()] += 1;
+        }
+        
+        $aProbability = $counts['a'] / $tries;
+        $bProbability = $counts['b'] / $tries;
+
+        $this->assertTrue($aProbability > 0.49);
+        $this->assertTrue($aProbability < 0.51);
+        $this->assertTrue($bProbability > 0.49);
+        $this->assertTrue($bProbability < 0.51);        
+    }
+    
+    public function testGettingTheVariationOfATestWithIrregularOddsOfVariations()
+    {
+        $tries  = 100000;
+        $test   = $this->getTest('test', array('a' => 1, 'b' => 2));
+        $test->run();
+        $counts = array('a' => 0, 'b' => 0);
+        
+        for ($i = 0; $i < $tries; $i++) {
+            $counts[$test->getVariation()] += 1;
+        }
+        
+        $aProbability = $counts['a'] / $tries;
+        $bProbability = $counts['b'] / $tries;
+
+        $this->assertTrue($aProbability > 0.32);
+        $this->assertTrue($aProbability < 0.34);
+        $this->assertTrue($bProbability > 0.66);
+        $this->assertTrue($bProbability < 0.68);
     }
 }
